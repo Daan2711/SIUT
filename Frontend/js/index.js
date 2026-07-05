9392101e0038eb2193c995a53cc56a8f303ca483
Daan2711 <danarr2711@gmail.com>
2026-07-05 00:12:57 -0600

    Update index.js

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -3,5 +3,15 @@ document.getElementById('btnLogout').addEventListener('click', function() {
     .then(() => {
         localStorage.removeItem('nombreUsuario');
         window.location.href = '/login';
+        const imagenesSlider = ["img/1.png", "img/2.png", "img/3.png", "img/4.png"];
+let indiceSlider = 0;
+
+function cambiarImagenSlider() {
+    indiceSlider = (indiceSlider + 1) % imagenesSlider.length;
+    document.getElementById("imagenSlider").src = imagenesSlider[indiceSlider];
+}
+
+setInterval(cambiarImagenSlider, 3000);
+
     });
 });
