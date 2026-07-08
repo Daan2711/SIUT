f551a740d2077f2732163580be8f2b6b5cd52ad8
Melanie128556 <marb2805@outlook.com>
2026-07-07 20:57:08 -0600

    Update index.js

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -5,13 +5,3 @@ document.getElementById('btnLogout').addEventListener('click', function() {
         window.location.href = '/login';
     });
 });
-
-const imagenesSlider = ["img/1.png", "img/2.png", "img/3.png", "img/4.png"];
-let indiceSlider = 0;
-
-function cambiarImagenSlider() {
-    indiceSlider = (indiceSlider + 1) % imagenesSlider.length;
-    document.getElementById("imagenSlider").src = imagenesSlider[indiceSlider];
-}
-
-setInterval(cambiarImagenSlider, 3000);
